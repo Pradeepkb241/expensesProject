c79940d04612c030edb0575a929fe5ebac31b06b
Pradeep <pradeepbanjare11@gmail.com>
2023-12-18 18:39:03 +0530

    adding submitHandler

--- src/components/NewExpense.js/ExpenseForm.js
+++ src/components/NewExpense.js/ExpenseForm.js
@@ -4,7 +4,7 @@ import './ExpenseForm.css';
 export default function ExpenseForm() {
     
     const[enteredTitle, setEnteredTitle]=useState('');
-    const[enteredAmoount, setEnteredAmount] = useState('');
+    const[enteredAmount, setEnteredAmount] = useState('');
     const[eneteredDate, setEnteredDate] = useState('');
     
     //Alternative
@@ -45,22 +45,35 @@ export default function ExpenseForm() {
         //     enteredDate: event.target.value
         // });
     }
+    const submitHandler = (event)=>{
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(eneteredDate)
+        }
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    }
   return (
-    <form >
+    <form onSubmit={submitHandler}>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>    
-                <input type="text" onChange={titleChangeHandler}/>
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
             </div>
 
             <div className='new-expense__control'>
                 <label>Amount</label>    
-                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
             </div>
 
             <div className='new-expense__control'>
                 <label>Date</label>    
-                <input type="date" min= '2023-01-01' max='2028-12-31' onChange={dateChangeHandler}/>
+                <input type="date" min= '2023-01-01' max='2028-12-31' value={eneteredDate} onChange={dateChangeHandler}/>
             </div>
 
         </div>
